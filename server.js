809773e467e90330a57eb76ed7a9d0055ff939ea
Jorge Dias <jorgedsdias@gmail.com>
2018-12-29 21:15:16 -0200

    segundo commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const http = require('http');
+
+const servidor = http.createServer(function(req, resp) {
+    let html = '';
+    if(req.url == '/') {
+        html = `
+            <html>
+                <head><meta charset="UTF-8"></head>
+                <body>
+                    <h1>Casa do Código</h1>
+                </body>
+            </html>
+        `;
+    } else if(req.url == '/livros') {
+        html = `
+            <html>
+                <head><meta charset="UTF-8"></head>
+                <body>
+                    <h1>Listagem de Livros</h1>
+                </body>
+            </html>
+        `;        
+    }
+
+    resp.end(html);
+});
+
+servidor.listen(3000);
