44b9044b8672d26bb5519c1eb7db84a058affb10
Jorge Dias <jorgedsdias@gmail.com>
2019-01-07 16:09:34 -0200

    ultimo commit

--- src/app/infra/livro-dao.js
+++ src/app/infra/livro-dao.js
@@ -40,6 +40,72 @@ class LivroDAO {
         });
     }
 
+    buscaPorId(id) {
+        return new Promise((resolve, reject) => {
+            this._db.get(
+                `
+                    SELECT *
+                    FROM livros
+                    WHERE id = ?
+                `,
+                [id],
+                (erro, livro) => {
+                    if(erro) {
+                        return reject('Nao foi possivel encontrar o livro!');
+                    }
+                    return resolve(livro);
+                }                
+            );
+        });
+    }
+
+    atualiza(livro) {
+        return new Promise((resolve, reject) => {
+            this._db.run(
+                `
+                    UPDATE livros SET
+                    titulo = ?,
+                    preco = ?,
+                    descricao = ?
+                    WHERE id = ?
+                `,
+                [
+                    livro.titulo,
+                    livro.preco,
+                    livro.descricao,
+                    livro.id
+                ],
+                (erro) => {
+                    if(erro) {
+                        return reject('Nao foi possivel atualizar o livro!');
+                    }
+                    resolve();
+                }                
+            );
+        });
+    }
+
+    remove(id) {
+        return new Promise((resolve, reject) => {
+            this._db.get(
+                `
+                    DELETE
+                    FROM livros
+                    WHERE id = ?
+                `,
+                [id],
+                (erro) => {
+                    console.log('x');
+                    if(erro) {
+                        console.log(erro);
+                        return reject('Nao foi possivel remover o livro!');
+                    }
+                    return resolve();
+                }                
+            );
+        });
+    }
+
 }
 
 module.exports = LivroDAO;
--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -29,7 +29,25 @@ module.exports = (app) => {
     });
 
     app.get('/livros/form', function(req, res) {
-        res.marko(require('../views/livros/form/form.marko'));
+        res.marko(require('../views/livros/form/form.marko'), 
+        {
+            livro: {}
+        });
+    });
+
+    app.get('/livros/form/:id', function(req, res) {
+        const id = req.params.id;
+        const livroDao = new LivroDAO(db);
+
+        livroDao.buscaPorId(id)
+        .then(livro => res.marko(
+                require('../views/livros/form/form.marko'), 
+                { 
+                    livro: livro 
+                }
+            )
+        )
+        .catch(erro => console.log(erro));
     });
 
     app.post('/livros', function(req, res) {
@@ -38,6 +56,24 @@ module.exports = (app) => {
         const livroDAO = new LivroDAO(db);
         livroDAO.adiciona(req.body)
         .then(res.redirect('/livros'))
+        .catch(erro => console.log(erro));
+    });
+
+    app.put('/livros', function(req, res) {
+        console.log(req.body);
+
+        const livroDAO = new LivroDAO(db);
+        livroDAO.atualiza(req.body)
+        .then(res.redirect('/livros'))
+        .catch(erro => console.log(erro));
+    });    
+
+    app.delete('/livros/:id', function(req, res) {
+        const id = req.params.id;
+
+        const livroDAO = new LivroDAO(db);
+        livroDAO.remove(id)
+        .then(() => res.status(200).end())
         .catch(erro => console.log(erro));
     });
 }
